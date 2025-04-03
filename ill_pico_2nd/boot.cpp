b0a97f39a170d31fb09fa251b2d2a70ad65453fc
filipeux <filipe2021.cruz@gmail.com>
2025-04-03 23:28:08 +0100

    BOOT cleaning the code still with errors

--- ill_pico_2nd/boot.cpp
+++ ill_pico_2nd/boot.cpp
@@ -24,65 +24,10 @@ void decodeCanId(uint16_t canId, uint8_t &sender, uint8_t &receiver, uint8_t &ta
     sender = canId & 0x07;
 }
 
-
-// // Constructor: Initialize buffer and attributes
-// boot::boot(uint16_t id, int size) : nodeId(id), bufferSize(size), bufferIndex(0) {
-//     buffer = new node_data[bufferSize];  // Allocate memory for buffer
-//     boot_init();
-// }
-
-// // Destructor: Free allocated memory
-// boot::~boot() {
-//     delete[] buffer;
-//     Serial.println("Boot buffer deallocated.");
-// }
-
-// // Initialize buffer
-// void boot::boot_init() {
-//     if (!buffer) {
-//         Serial.println("Memory allocation failed!");
-//         return;
-//     }
-//     Serial.print("Buffer initialized with size: ");
-//     Serial.println(bufferSize);
-// }
-
-// // CAN Message Handling
-// void boot::NODE_BOOT(canbus_comm* hermes) {
-//     unsigned long startTime = millis();
-//     srand(micros());
-//     uint8_t randomValue = rand() % 256;
-
-//     uint16_t id = encodeCanId(0, 0, 0, 0);
-//     msg_to_can inner_frm_core0;
-
-//     // Send initial message
-//     hermes->send_msg(id, HEAD_FLAG, randomValue, &inner_frm_core0);
-
-//     // Listen for messages for 2 seconds
-//     while (millis() - startTime < 2000) {
-//         if(hermes->recv_msg(&inner_frm_core0)){
-//             hermes->process_msg_core0(&inner_frm_core0);
-//             uint16_t receivedId = inner_frm_core0.wrapped.can_id;
-
-//             // Store in buffer if ID is different from node ID
-//             if (receiv<<edId != nodeId && bufferIndex < bufferSize) {
-//                 buffer[bufferIndex++] = inner_frm_core0;
-//                 std::cout << "Stored CAN Message with ID: " << receivedId << std::endl;
-//             }
-
-//             Serial.println(inner_frm_core0.wrapped.can_msg);
-//             Serial.println(inner_frm_core0.wrapped.can_id);
-//             }
-//         }
-// }
-
-
-
 void boot::NODE_BOOT(canbus_comm* hermes,msg_to_can inner_frm_core0) {
     unsigned long startTime;
     srand(micros()); // Seed random generator
-    uint8_t randomValue;
+    uint32_t randomValue;
     bool idConflict;
     int conflits = 0;
 
@@ -108,14 +53,18 @@ void boot::NODE_BOOT(canbus_comm* hermes,msg_to_can inner_frm_core0) {
         continue;
     }
     
-    
+    Serial.println("\nInitial Node Buffer:");
+    for (int i = 0; i < bufferSize; i++) {
+      Serial.print("Node ID: ");
+      Serial.println(nodeBuffer[i].id);
 
     // Listen for responses for 2 seconds
     startTime = millis();
     while (millis() - startTime < 5000) {
         if (hermes->recv_msg(&received_msg)) {
             hermes->process_msg_core0(&received_msg);
-            uint16_t receivedId = received_msg.wrapped.can_msg.can_id;
+            uint32_t receivedId = received_msg.wrapped.can_msg.can_id;
+            Serial.println("MESSAGE RCV:");
             Serial.println(receivedId);
 
             // If another node has the same ID, regenerate
@@ -125,11 +74,12 @@ void boot::NODE_BOOT(canbus_comm* hermes,msg_to_can inner_frm_core0) {
                 break;  // Exit early to regenerate ID
             }
             
-            // // Store the received message if it's from another node
-            // if (receivedId != nodeId && bufferIndex < bufferSize) {
-            //     //buffer[bufferIndex++] = {receivedId, received_msg}; // Store message in buffer
-            //     // std::cout << "Stored CAN Message from node ID: " << receivedId << std::endl;
-            // }
+
+            Serial.println("\n Middle Node Buffer:");
+            for (int i = 0; i < bufferSize; i++) {
+              Serial.print("Node ID: ");
+              Serial.println(nodeBuffer[i].id);
+            }
 
 
             // Check if message is already in the buffer
@@ -166,7 +116,12 @@ void boot::NODE_BOOT(canbus_comm* hermes,msg_to_can inner_frm_core0) {
         }
     }
     do {
-        //place the verification of the messages seeing the 
+      /*
+      
+      
+      
+      */
+       //place the verification of the messages seeing the 
     } while (idConflict);  // Keep retrying until a unique ID is assigned
     while (conflits>0)
     {
@@ -179,29 +134,11 @@ void boot::NODE_BOOT(canbus_comm* hermes,msg_to_can inner_frm_core0) {
     Serial.println(nodeId);
 
 
-      Serial.println("\nFinal Node Buffer:");
-    for (int i = 0; i < bufferSize; i++) {
-      Serial.print("Node ID: ");
-      Serial.print(nodeBuffer[i].id);
+    // Serial.println("\nFinal Node Buffer:");
+    // for (int i = 0; i < bufferSize; i++) {
+    //   Serial.print("Node ID: ");
+    //   Serial.print(nodeBuffer[i].id);
       // Serial.print(" | G: ");
       // Serial.println(nodeBuffer[i].G, 6); // Print float with 6 decimal places
   }
 }
-
-
-
-
-// void NODE_BOOT(){
-//     unsigned long startTime = millis();
-//     srand(micros());
-//     uint8_t randomValue = rand() % 256;
-//     uint16_t id = encodeCanId(0,0,0,0);
-  
-//     hermes.send_msg(id,HEAD_FLAG,randomValue,&inner_frm_core0);
-  
-//     while(millis() - startTime < 2000){
-//       hermes.recv_msg(&inner_frm_core0);
-//       hermes.process_msg_core0(&inner_frm_core0);
-  
-//     }
-//   }
